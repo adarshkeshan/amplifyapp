049c60a042c69c30a38b62d30061f5779e4331b6
Adarsh keshan <adkeshan@amazon.com>
2023-04-20 16:44:21 -0700

    Changes

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyappcbd9a1f4": {
       "AppClientID": "string",
