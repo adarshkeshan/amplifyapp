38999e698b4946632176f2620c7e8ba8281d7820
Adarsh keshan <adkeshan@amazon.com>
2023-04-21 10:57:27 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -12,114 +12,44 @@ import {
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
-import { listNotes } from "./graphql/queries";
+import { listNotes,listAdlines } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
 
 const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setAdLines] = useState([]);
 
   useEffect(() => {
-    fetchNotes();
+    fetchAdLines();
   }, []);
-async function fetchNotes() {
-  const apiData = await API.graphql({ query: listNotes });
-  const notesFromAPI = apiData.data.listNotes.items;
-  await Promise.all(
-    notesFromAPI.map(async (note) => {
-      if (note.image) {
-        const url = await Storage.get(note.name);
-        note.image = url;
-      }
-      return note;
-    })
-  );
-  setNotes(notesFromAPI);
+
+async function fetchAdLines() {
+  const apiData = await API.graphql({ query: listAdlines });
+  const notesFromAPI = apiData.data.listAdlines.items;
+  setAdLines(notesFromAPI);
 }
-async function createNote(event) {
-  event.preventDefault();
-  const form = new FormData(event.target);
-  const image = form.get("image");
-  const data = {
-    name: form.get("name"),
-    description: form.get("description"),
-    image: image.name,
-  };
-  if (!!data.image) await Storage.put(data.name, image);
-  await API.graphql({
-    query: createNoteMutation,
-    variables: { input: data },
-  });
-  fetchNotes();
-  event.target.reset();
-}
-async function deleteNote({ id, name }) {
-  const newNotes = notes.filter((note) => note.id !== id);
-  setNotes(newNotes);
-  await Storage.remove(name);
-  await API.graphql({
-    query: deleteNoteMutation,
-    variables: { input: { id } },
-  });
-}
+  
 
   return (
     <View className="App">
       <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <View
-            name="image"
-            as="input"
-            type="file"
-            style={{ alignSelf: "end" }}
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
+      <Heading level={2}>Current AdLines</Heading>
       <View margin="3rem 0">
+        
 {notes.map((note) => (
   <Flex
-    key={note.id || note.name}
+    key={note.dim_ad_id}
     direction="row"
     justifyContent="center"
     alignItems="center"
   >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
+    <Text as="span">{note.ad_name}</Text>
+    <Text as="span">{note.dim_ad_id}</Text>
+    <Text as="span">{note.campaign_name}</Text>
+    <Text as="span">{note.advertiser_name}</Text>
+    <Text as="span">{note.campaign_goal}</Text>
   </Flex>
 ))}
       </View>
